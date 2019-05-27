6bd7619175558d148c23a14a3570e1e2a8ae25c3
Tim Knoll <timothy.knoll@gmail.com>
2019-05-27 16:01:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,6 +30,7 @@ class Student
   end   
 
   def save
+    # instance method inserts a new row into the database using attributes of given object
     if self.id
       self.update
     else
@@ -38,23 +39,26 @@ class Student
       SQL
     
       DB[:conn].execute(sql,self.name,self.grade)
-    
+    # assigns id attribute of the object once the row has inserted into database
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end   
   end 
   
   def self.create (name, grade)
+    # method creates a student with name and grade attribute then saves into students table
     student = self.new(name,grade)
     student.save
     student
   end   
 
   def self.new_from_db(row)
+    #class method uses three array elements (id,name,grade) to create a new Student object
     student = self.new(row[1],row[2],row[0])
     student
   end
   
   def self.find_by_name(name)
+    #class method take name argument; queries database table for record then uses the new_from_db method to instantiate a Student object with database row the SQL query returns
     sql = <<-SQL
       SELECT * FROM students WHERE name = ? LIMIT 1 
     SQL
@@ -64,7 +68,8 @@ class Student
     end.first        
   end   
   
-  def update #
+  def update 
+    # method updates database row mapped to given Student instance
     sql = <<-SQL
       UPDATE students SET name = ?, grade = ? WHERE id = ?
       SQL
